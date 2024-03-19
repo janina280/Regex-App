ff53217c3cea149ed0a715e04451e9c21ec017a1
Janina <coceijanina@yahoo.com>
2024-03-19 19:25:43 +0200

    Fix app

---
+++ LFA_Application/LFA_Application/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using LFA_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace LFA_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new Form();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Form model)
+        {
+            if (string.IsNullOrEmpty(model.Pattern) && string.IsNullOrEmpty(model.Text1) &&
+                string.IsNullOrEmpty(model.Text2))
+            {
+                return View(model);
+            }
+
+            if (!IsValidRegexPattern(model.Pattern))
+            {
+                ViewBag.Error = "Pattern-ul introdus nu este valid.";
+                return View(model);
+            }
+            else
+            {
+                ViewBag.Error = "";
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            Regex regex = new Regex(model.Pattern);
+
+            var forText1 = regex.Match(model.Text1);
+            if (forText1.Success)
+            {
+                model.Matched = "Matched";
+            }
+            else
+            {
+                model.Matched = "DisMatched";
+            }
+
+            var forText2 = regex.Matches(model.Text2);
+
+            foreach (Match match in forText2)
+            {
+                if (string.IsNullOrEmpty(model.Matches))
+                {
+                    model.Matches = match.ToString();
+                }
+                else
+                {
+                    model.Matches = model.Matches + ", " + match.ToString();
+                }
+            }
+
+            return View(model);
+        }
+
+
+        private bool IsValidRegexPattern(string pattern)
+        {
+            try
+            {
+                Regex.Match("", pattern);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ LFA_Application/LFA_Application/Models/Form.cs
@@ -0,0 +1,12 @@
+﻿namespace LFA_Application.Models;
+
+public class Form
+{
+    public string Pattern { get; set; } = string.Empty;
+    public string Text1 { get; set; } = string.Empty;
+    public string Text2 { get; set; } = string.Empty;
+    public string Matched { get; set; } = "Not verified";
+    public string Matches { get; set; } = string.Empty;
+
+
+}
